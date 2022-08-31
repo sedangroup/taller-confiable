5270210892f7fb466ded081c2317a75ca5f49c3d
andresAgudelo0101 <echeverry11298@gmail.com>
2022-08-31 17:06:40 -0500

    creamos el layout del auxiliar

---
+++ proyecto-taller-alto-nivel/proyecto-taller-alto-nivel/Controllers/AuxiliarController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace proyecto_taller_alto_nivel.Controllers
+{
+    public class AuxiliarController : Controller
+    {
+        public IActionResult GuardarMecanico()
+        {
+            return View();
+        }
+
+        public IActionResult GuardarVehiculo()
+        {
+            return View();
+        }
+
+    }
+}
