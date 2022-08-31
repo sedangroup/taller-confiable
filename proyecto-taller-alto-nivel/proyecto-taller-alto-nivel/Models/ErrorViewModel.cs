1e2d8f42b8f98130bdee4f7e2f2eeef7aa8efc2a
andresAgudelo0101 <echeverry11298@gmail.com>
2022-08-31 12:43:41 -0500

    creamos el proyecto inicial

---
+++ proyecto-taller-alto-nivel/proyecto-taller-alto-nivel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace proyecto_taller_alto_nivel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
