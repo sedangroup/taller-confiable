bd884b5eb25c26a130a8257786c2f60ed74778a3
andresAgudelo0101 <echeverry11298@gmail.com>
2022-09-13 15:19:53 -0500

    controllers views propietario,mecanico

---
+++ proyecto-taller-alto-nivel/proyecto-taller-alto-nivel/Controllers/MecanicoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace proyecto_taller_alto_nivel.Controllers
+{
+    public class MecanicoController : Controller
+    {
+        // GET: MecanicoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
