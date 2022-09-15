5840e37153226d9bea7eb05c33badba2929a882d
andresAgudelo0101 <echeverry11298@gmail.com>
2022-09-15 08:15:42 -0500

    listar servicio mecanico funciona

--- proyecto-taller-alto-nivel/proyecto-taller-alto-nivel/Controllers/MecanicoController.cs
+++ proyecto-taller-alto-nivel/proyecto-taller-alto-nivel/Controllers/MecanicoController.cs
@@ -1,16 +1,30 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using proyecto_taller_alto_nivel.Data;
+using proyecto_taller_alto_nivel.Models;
 
 namespace proyecto_taller_alto_nivel.Controllers
 {
     public class MecanicoController : Controller
     {
+        ServicioDatos Servicio_Datos = new ServicioDatos();
         // GET: MecanicoController
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult ListaServiciosMecanico(MecanicoModel mecanico)
+        {
+            var list = Servicio_Datos.ListarByDocumento(mecanico);
+            return View(list);
+        }
+
+        public ActionResult ObtenerServicioDocumento()
+        {
+            
+            return View();
+        }
 
     }
 }
